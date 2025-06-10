27775cad91be2f010334633280d2e940cc07501a
Iago Perez Fernandez <68069957+Winter-23@users.noreply.github.com>
2025-06-10 09:33:56 +0100

    Update forecast.py
    I updated the forecast data to measure the weekly mean of max, mean and daily mean temperature as it was done in the ERA5 DART pipeline

--- src/dart_bias_correct/forecast.py
+++ src/dart_bias_correct/forecast.py
@@ -240,20 +240,18 @@ def get_weekly_forecast(data_raw_forecast: xr.Dataset) -> xr.Dataset:
 
     # Weekly aggregation
     # TODO: Note that this is different from aggregation in the primary pipeline (DART-Pipeline)
-    #       where maximum is taken over the day and overall mean is calculated
+    #       where maximum is taken over the day and overall mean is calculated ##Iago: It should follow the same methodology as in the primary 
     weekly_mean = data_raw_forecast_inst.resample(step="7D").mean(dim="step")[
         ["t2m", "r", "q"]
     ]
     weekly_max = (
-        data_raw_forecast_inst.resample(step="7D")
-        .max(dim="step")
+        data_raw_forecast_inst.resample(step="1D").max(dim="step").resample(step="7D").mean(dim="step")
         .rename_vars({"t2m": "mx2t24", "r": "mxr24", "q": "mxq24"})[
             ["mx2t24", "mxr24", "mxq24"]
         ]
     )
     weekly_min = (
-        data_raw_forecast_inst.resample(step="7D")
-        .min(dim="step")
+        data_raw_forecast_inst.resample(step="1D").min(dim="step").resample(step="7D").mean(dim="step")
         .rename_vars({"t2m": "mn2t24", "r": "mnr24", "q": "mnq24"})[
             ["mn2t24", "mnr24", "mnq24"]
         ]
